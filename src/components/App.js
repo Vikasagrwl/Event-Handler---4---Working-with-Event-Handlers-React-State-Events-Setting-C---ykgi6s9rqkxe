a48b65f4e7010ae42e4ce2e508d9d80c5f3d199e
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-02-23 19:35:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
+  const [msg, setMsg] = useState("I was not double clicked");
 
-  const handleDoubleClick = (event) =>{
-  
-  }
+  const handleDoubleClick = (event) => {
+    setMsg("I was double clicked");
+  };
+
+  console.log(msg);
+
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button id="dblclick-btn" onDoubleClick={handleDoubleClick}>
+        Double click me
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
